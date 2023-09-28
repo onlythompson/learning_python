b63565ef3fdd751570fcb68303efc2cf14600f4f
onlythompson <onlythompson@gmail.com>
2023-09-27 21:02:39 -0700

    27-09-23 Sesssion

--- concept_of_functions_1.py
+++ concept_of_functions_1.py
@@ -9,10 +9,15 @@ def function_name(args):
     <statements>
 """
 def doSomething():
-    print("I am a function")#statement
+    print("I am a function")#statement- calling the print function
 
 def doAnotherThing(name:str):
-    print("I am  "+ name)
+    print("I am  "+ name) #calling the print function
+
+def doTheThirdThing(name:str, height:float):
+    #print("I am  "+ name) #calling the print function
+    doAnotherThing(name)#calling the doAnotherThing function
+    print("I am "+ str(height) + "meters")#calling the print function
 
 #calling a function
 """
@@ -20,7 +25,12 @@ Syntax for calling a function
 
 function_name()
 """
-doSomething()
-doAnotherThing("Joseph")
+doSomething()#no argument calling the doSomething function
+doAnotherThing("Joseph")# 1 argument calling the doAnotherThing function
+doTheThirdThing("Okon", 1.74)# 2 argument calling the doTheThirdThing function
 
 
+def doTheFourthThing():
+    doAnotherThing("Amanam")#calling the print doAnotherThing function
+
+doTheFourthThing()
